2f2a56c4b79f15ea2fbea46bf0cdd0774281527c
kkopoku <kk.opoku@outlook.com>
2025-03-03 12:44:42 +0000

    chore: improved text processing by model

--- text-processor/app/gemini_service.py
+++ text-processor/app/gemini_service.py
@@ -11,6 +11,7 @@ def process_text(text: str) -> str:
 
     try:
         input = f"""
+            You are an advanced model used to extract some params from text to form an actionable JSON.
             Extract details of a money transfer from the following text and return a JSON object with these keys:
             - `to`: The recipient's phone number.
             - `mno`: The mobile network operator (must be one of 'Zeepay', 'MTN', 'Vodafone', or 'AirtelTigo').
@@ -18,8 +19,14 @@ def process_text(text: str) -> str:
 
             Rules:
             1. If all three parameters (`to`, `mno`, and `amount`) can be accurately extracted, return a properly formatted JSON object.
-            2. If any of the parameters are missing or unclear or null, return NULL instead instead of JSON. Make sure you all parameters are extracted else return NULL.
-            3. Only process messages related to sending money. If the text is unrelated, return UNRELATED.
+            2. If any of the parameters are missing but the text is related to sending money, return NULL instead instead of JSON. Make sure you all parameters are extracted else return NULL.
+            3. If the text doesnt sound like it is around sending money, return UNRELATED.
+            4. When any of the parameters are missing, return NULL.
+            
+            ie. send 50 cedis to Kwame should return NULL because it doesnt state anything related to mno
+            ie. send to Kwame should return NULL because it doesnt state anything related to mno, amount
+            ie. send 50 cedis to MTN should return NULL because it is missing the to param
+            ie  send shoud return NULL because all the params are missing
 
             Here is the JSON properties data structure to=string mno=string amount=number
 
